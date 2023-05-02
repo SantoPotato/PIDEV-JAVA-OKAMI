705e2ed35aeb600e3f9653e16dd361bb43ef5969
azizhannachi12557 <80336529+azizhannachi12557@users.noreply.github.com>
2023-05-02 22:09:15 +0200

    Revert "COMMIT"
    This reverts commit f7958195c2b7f2e6fd139159bc77f26bbe470d80.

---
+++ src/tests/MainClass.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests;
+
+import services.userCRUD;
+import utils.MyConnexion;
+
+/**
+ *
+ * @author SONDESH
+ */
+public class MainClass {
+   public static void main (String[] args){
+   //MyConnexion mc = new  MyConnexion ();
+   userCRUD pcd = new userCRUD();
+  // pcd.ajouteruser();
+  //System.out.println(pcd.afficherusers());
+    //Modification d'un user
+    // pcd.updateuser(56,"taraji","dawla","kabbaria","kabbaria");
+  //delet
+  //pcd.deleteuser(35);
+}}
