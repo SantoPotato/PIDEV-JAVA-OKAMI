59e56b066d28b049d1db5b30d8c311c05abf7b51
amine722 <114940080+amine722@users.noreply.github.com>
2023-05-02 09:26:04 +0100

    h

---
+++ src/GUIStock/AjoutStockController.java
@@ -0,0 +1,167 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUIStock;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.Date;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import stock.entities.Stock;
+import stock.entities.Stockcategories;
+import stock.services.StockcategoriesCRUD;
+import stock.services.StockCRUD;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.util.StringConverter;
+import stock.utils.MyDB;
+
+
+
+
+
+/**
+ * FXML Controller class
+ *
+ * @author SNAKE 2-16
+ */
+public class AjoutStockController implements Initializable {
+
+  Connection c;
+    @FXML
+    private Label labelPage;
+    @FXML
+    private Label labelPath;
+    @FXML
+    private Label labelIndex;
+    @FXML
+    private ImageView logo;
+    @FXML
+    private Button buttonTest;
+    @FXML
+    private Button buttonAdd;
+    @FXML
+    private TextField nomeq;
+    @FXML
+    private TextField nomeq1;
+    @FXML
+    private DatePicker dat;
+    
+    
+    @FXML
+    
+    
+    
+    private ChoiceBox<Stockcategories>  catColumn;
+    private TextField nomeq2;
+    @FXML
+    private Button buttonRendezvous;
+    @FXML
+    private Button buttonRendezvousType;
+    
+    @FXML
+    private TextField nomq2;
+    private List<Stockcategories> types = new ArrayList<>();
+    
+
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        
+         c = MyDB.getInstance().getCnx();
+        
+        
+        StockcategoriesCRUD S = new StockcategoriesCRUD();
+
+        types = S.Afficherc();
+
+        // Ajout des noms des types dans la choiceBox
+        catColumn.getItems().addAll(types);
+
+        // Définition de la façon dont les noms des types seront affichés dans la choiceBox
+        catColumn.setConverter(new StringConverter<Stockcategories>() {
+            public String toString(Stockcategories type) {
+                return type.getTypecat();
+            }
+
+            public Stockcategories fromString(String string) {
+                return catColumn.getItems().stream().filter(type ->
+                        type.getTypecat().equals(string)).findFirst().orElse(null);
+            }
+        });
+        
+        
+    }
+
+@FXML
+    private void stockac(ActionEvent event) {
+         try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Details.fxml"));
+            labelIndex.getScene().setRoot(loader.load());
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+@FXML
+private void StockAdd(ActionEvent event) {
+    
+    String nomst = nomeq.getText();
+    String description = nomeq1.getText();
+    LocalDate dateexpirationst = dat.getValue();
+    Stockcategories stockcat_id = catColumn.getSelectionModel().getSelectedItem();
+    int quantites = Integer.parseInt(nomq2.getText());
+    
+ 
+
+    Stock e = new Stock();
+    StockCRUD ec = new StockCRUD();
+    e.setNomst(nomst);
+    e.setDescription(description);
+    e.setDateexpirationst(dateexpirationst);
+    e.setStockcat_id(stockcat_id);
+    e.setQuantites(quantites);
+    ec.Ajouter(e);
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("Details.fxml"));
+    try {
+        Parent root = loader.load();
+        buttonAdd.getScene().setRoot(root);
+    } catch (IOException ex) {
+        System.out.println(ex.getMessage());
+    }
+}
+
+
+}
+
+
+
+
+   
+    
+
